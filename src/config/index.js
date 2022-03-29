3a829b1d6f3507e74ea8f4b448c17b82ea94d113
arivera <andresrfelip@hotmail.com>
2022-03-28 21:59:29 -0500

    configuración archivo index para dotenv

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+
+const envFound = dotenv.config();
+
+// sino se encuentra archivo .env muestra el siguiente error
+if(!envFound){
+    throw new Error("Couldn't find .env file.");
+}
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'developmennt';
+
+module.exports = {
+    port: process.env.PORT,
+    api:{
+        prefix: '/api/v1',
+    }
+}
